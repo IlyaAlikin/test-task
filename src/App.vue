ab0bc3b0de394f30160937c5fca32ac65cf78995
Ilya Alikin <iljuha.alikin@gmail.com>
2024-08-01 14:40:03 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template></template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import axios from "axios";
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get("https://swapi.dev/api/");
+    console.log(response.data); // Access fetched data here
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+<style scoped></style>
---
+++ src/components/Select.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import axios from "axios";
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get("https://swapi.dev/api/starships/");
+    console.log(response.data); // Access fetched data here
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+<style scoped></style>
